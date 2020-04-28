8b69cf98eb8ab0caf0dcfd73a7d1a912e5cf4a9d
Kartik Patidar <kartikpatidar815@gmail.com>
2020-04-28 20:56:36 +0530

    styling the app

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is Help text',
+        helpText: 'For any queries contact the developer.',
         title: 'Help Page',
         name: 'Kartik'
     })
